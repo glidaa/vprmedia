f18ac3e1c5b94ea917c3f56321c92c7a78559b77
mc-usercentric <68213914+mc-usercentric@users.noreply.github.com>
2020-08-11 19:47:59 +1000

    Updates

---
+++ precache-manifest.86e45d702640bb2427879f57a883f884.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0fe690d273750c60e6417accde62b83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6f746c809f202064f3c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6f746c809f202064f3c",
+    "url": "/static/js/main.db69d7ad.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
